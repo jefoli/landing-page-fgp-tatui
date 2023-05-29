9e86775eff1c8d83f306d4f1b356c4ed12044789
Jeferson Oliveira <jheferson.contato@outlook.com>
2023-05-29 12:45:32 -0300

    atualização da historia da fgp

--- src/Components/History/index.jsx
+++ src/Components/History/index.jsx
@@ -5,48 +5,46 @@ export const History = () => {
     <section className="section-container-image">
       <div className="test12 divflex">
         <h1>CONHEÇA NOSSA HISTÓRIA</h1>
-        <h2>a FGP polo Tatuí é uma referência no setor educacional</h2>
-        <h5>
-          A FGP polo Tatuí é a continuidade de um projeto que teve início em
-          2012 com um nome um pouco diferente, mas a missão continua sendo
-          oferecer conhecimento e qualidade de ensino para nossos estudantes.
-        </h5>
 
-        <h5>
-          Isso permitiu que a empresa alcançasse um número ainda maior de
-          educadores e, consequentemente, impactar a educação em nossa região.
-        </h5>
+        <div>
+          <p>
+            A FGP Polo Tatuí é uma referência no setor educacional. Desde 2012,
+            estamos comprometidos em oferecer conhecimento e qualidade de ensino
+            aos nossos estudantes.
+          </p>
+          <p>
+            Com o passar do tempo, expandimos nosso polo para outras regiões,
+            conquistando a confiança de alunos de cidades como Iperó, Laranjal
+            Paulista, Tietê e Boituva.
+          </p>
+          <p>
+            Continuamos evoluindo e inovando, incorporando tecnologias
+            emergentes, como inteligência artificial e aprendizado adaptativo,
+            em nossa plataforma. Isso nos permitiu alcançar um número ainda
+            maior de educadores e impactar a educação em nossa região.
+          </p>
+          <p>
+            Contamos com uma equipe de docentes composta por Mestres ou Doutores
+            em suas respectivas áreas de atuação. Com toda essa bagagem de
+            conhecimento, nossos docentes capacitam os alunos, indo além das
+            simples aulas, proporcionando o uso de ferramentas essenciais para o
+            ensino-aprendizagem.
+          </p>
+          <p>
+            Nosso compromisso em capacitar e apoiar os professores permanece
+            inabalável, pois eles continuam a moldar o futuro da educação em
+            todo o mundo.
+          </p>
+        </div>
 
-        <h5>
-          contamos com a equipe de docentes de Mestres ou Doutores em sua área
-          de atuação. Com toda essa bagagem de conhecimento, nossos (as)
-          docentes capacitam os alunos, muito além de apenas assistir aulas, mas
-          fazer uso das ferramentas que serão essenciais para o ensino
-          aprendizagem. Com o passar do tempo, expandimos nosso polo para outras
-          regiões, onde conquistamos a confiança de alunos de outras cidades
-          (Iperó, Laranjal Paulista, Tietê e Boituva).
-        </h5>
-        <h5>
-          continuamos a evoluir e inovar, incorporando tecnologias emergentes,
-          como inteligência artificial e aprendizado adaptativo em sua
-          plataforma.
-        </h5>
-        <h5>
-          Nosso compromisso em capacitar e apoiar os professores permanece
-          inabalável, à medida que eles continuam a moldar o futuro da educação
-          em todo o mundo.
-        </h5>
         <div>
           <h2>NOSSA MISSÃO</h2>
-          <div>
-            <h5>
-              A nossa missão é desenvolver competências e habilidades,
-              direcionada a formação e orientação dos profissionais compatíveis
-              com as exigências do seu cotidiano, promovendo o desenvolvimento
-              contínuo e a transformação da sociedade por meio da assimilação de
-              novos conhecimentos.
-            </h5>
-          </div>
+          <p>
+            Nossa missão é desenvolver competências e habilidades, formar e
+            orientar profissionais alinhados com as exigências do cotidiano,
+            promovendo o desenvolvimento contínuo e a transformação da sociedade
+            por meio da assimilação de novos conhecimentos.
+          </p>
         </div>
 
         <div>
@@ -55,27 +53,27 @@ export const History = () => {
             <div>
               <FaGraduationCap />
               <h3>excelência acadêmica</h3>
-              <h6>
+              <p>
                 Promovendo a busca pela excelência acadêmica e preparando os
                 alunos para suas futuras carreiras.
-              </h6>
+              </p>
             </div>
             <div>
               <FaUsers />
               <h3>diversidade e inclusão</h3>
-              <h6>
+              <p>
                 Valorizando a diversidade em todas as suas formas e criando um
                 ambiente inclusivo para todos os alunos.
-              </h6>
+              </p>
             </div>
             <div>
               <FaLightbulb />
               <h3>inovação e pesquisa</h3>
-              <h6>
+              <p>
                 Fomentando a inovação e a pesquisa em diversas áreas de
                 conhecimento, incentivando a descoberta de novos conhecimentos e
                 avanços.
-              </h6>
+              </p>
             </div>
           </div>
         </div>
