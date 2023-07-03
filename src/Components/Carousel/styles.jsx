56f8e51b22bd6b87bcba304de5f6d1c99824d296
Jeferson Oliveira <jheferson.contato@hotmail.com>
2023-07-03 14:57:33 -0300

    teste scroll

--- src/Components/Carousel/styles.jsx
+++ src/Components/Carousel/styles.jsx
@@ -2,21 +2,20 @@ import styled, { css } from 'styled-components';
 
 export const CarouselWrapper = styled.div`
   ${({ theme }) => css`
+    display: flex;
+    overflow: hidden;
+    height: 100%;
+    position: relative;
 
-  display: flex;
-  overflow: hidden;
-  height: 100%;
-  position: relative;
-
-   & p {
-    font-size: calc(${theme.fonts.sizes.small} + 0.2rem );
-    font-weight: 300;
-   }
-  @media (max-width: 768px) {
-    flex-direction: flex;
-    align-items: center;
-    min-height: 100%;
-  }
+    & p {
+      font-size: calc(${theme.fonts.sizes.small} + 0.2rem );
+      font-weight: 300;
+    }
+    @media (max-width: 768px) {
+      flex-direction: flex;
+      align-items: center;
+      min-height: 100%;
+    }
   `}
 `;
 
@@ -44,7 +43,7 @@ export const Button = styled.div`
     border-radius: 100%;
     cursor: pointer;
     font-size: ${theme.fonts.sizes.xlarge};
-    padding: 0 ${theme.fonts.sizes.large};
+    padding: ${theme.fonts.sizes.xxhuge} ${theme.fonts.sizes.large};
   `}
 `;
 
@@ -52,4 +51,6 @@ export const Slide = styled.div`
   flex: 0 0 25%;
   transition: transform 0.3s ease-in-out;
   scroll-snap-type: x mandatory;
+  scroll-snap-type: y proximity;
+  scroll-snap-type: both mandatory;
 `;
